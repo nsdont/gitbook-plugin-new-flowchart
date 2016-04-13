a96638a57dd83d824c6e64c43bd66f78586ecc23
NSDont <haodewanan@gmail.com>
2016-04-13 17:20:49 +0800

    Modify package and plugin.js

--- book/plugin.js
+++ book/plugin.js
@@ -1,11 +1,9 @@
 require(["gitbook"], function(gitbook) {
   gitbook.events.bind("page.change", function() {
-    $('code.lang-flowchart').each(function(index, element) {
+    $('code.lang-flow').each(function(index, element) {
       var $element = $(element),
-          code = $element.value,
+          code = $element.text(),
           chart;
-      console.log($element.value);
-      console.log(code);
       
       var wrapper = $("<div id='canvas'></div>");
       $element.parent().replaceWith(wrapper);
