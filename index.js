b3ee7fbe7c6b2fde8561e0478b859039a4ebd5bc
Johan Preynat <johan.preynat@gmail.com>
2016-04-19 12:05:24 +0200

    Include raphaeljs using gitbook 3 templating

--- index.js
+++ index.js
@@ -4,12 +4,6 @@ module.exports = {
         js: [
             "flowchart-1.6.2.js",
             "plugin.js"
-        ],
-        html: {
-            "head:end": function() {
-                // Required to including this way because raphael is non-AMD compliant
-                return "<script src='//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.2/raphael-min.js'></script>";
-            }
-        }
+        ]
     }
 };
